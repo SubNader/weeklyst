a6425dc6864a662780303ed4dac0d86e6fcc2969
Nader Elsharkawy <subnader@gmail.com>
2025-05-01 12:52:52 +0200

    Update package

--- vue.config.js
+++ vue.config.js
@@ -4,12 +4,11 @@ module.exports = {
       nodeIntegration: true,
       customFileProtocol: './',
       builderOptions: {
-        appId: "Weeklyst-app.netlify.app",
-        productName: "Weeklyst",
+        appId: "weeklyst.com",
+        productName: "weeklyst",
         publish: ["github"],
         linux: {
           category: "Utility",
-          description: "Free and Open Source Minimalist Weekly Planner and To Do list App focused on privacy.",
           target: ["deb", "rpm", "pacman","AppImage"],
           icon: "build/icon.icns",
         },
