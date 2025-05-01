684706cfef565a98973ea02d6bf5243178b7a52e
Nader Elsharkawy <subnader@gmail.com>
2025-05-01 12:55:23 +0200

    Update Arabic

--- src/views/welcome/languageView.vue
+++ src/views/welcome/languageView.vue
@@ -26,7 +26,7 @@
             <option value="hi">हिंदी</option>
             <option value="ja">日本</option>
             <option value="pl">Polski</option>
-            <option value="ar">عرب</option>
+            <option value="ar">عربي</option>
             <option value="ko">한국어</option>
             <option value="zh_cn">简体中文</option>
             <option value="zh_tw">繁體中文</option>
